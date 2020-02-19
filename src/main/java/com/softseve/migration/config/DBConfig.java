8baaa18cf2135e49a416a8e243f4f7d3d8b5c433
RomanAndriienko <andriienkoroman@gmail.com>
2020-02-19 17:42:24 +0200

    add DBConfig

---
+++ src/main/java/com/softseve/migration/config/DBConfig.java
@@ -0,0 +1,26 @@
+package com.softseve.migration.config;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder
+            .setType(EmbeddedDatabaseType.HSQL)
+            .addScript("db/sql/schema.sql")
+            .build();
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
