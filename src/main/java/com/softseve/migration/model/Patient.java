a6ad6d6e7c68bf5b4166c9c25e241760c08cba7e
iholovata <irina-golovataja@ukr.net>
2020-02-19 18:44:35 +0200

    model added

--- src/main/java/com/softseve/migration/model/Patient.java
+++ src/main/java/com/softseve/migration/model/Patient.java
@@ -1,5 +1,21 @@
 package com.softseve.migration.model;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class Patient {
 
+    private UUID id;
+    private String bDate;
+    private String refId;
+    private String accessDate;
+    private int items;
+    private int MPI;
+    private String patientTypeId;
+    private String patientTypeTxt;
+    private UUID patientTypeRef;
+    private String cDateTime;
+    private String uDateTime;
 }
---
+++ src/main/java/com/softseve/migration/model/PatientContact.java
@@ -0,0 +1,26 @@
+package com.softseve.migration.model;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class PatientContact {
+
+    private UUID id;
+    private String cDate;
+    private String contactRef;
+    private String pCode;
+    private String firstName;
+    private String lastName;
+    private String user;
+    private int facility;
+    private int cntRef;
+    private int cntRef2;
+    private String contactTypeId;
+    private String contactType;
+    private UUID contactTypeIdx;
+    private int sum;
+    private String cDateTime;
+    private String uDateTime;
+}
