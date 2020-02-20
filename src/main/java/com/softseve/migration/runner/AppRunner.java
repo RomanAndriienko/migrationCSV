34a6f2bd25d99058b9e86a06b32cd425ddfc15ff
RomanAndriienko <andriienkoroman@gmail.com>
2020-02-20 17:55:17 +0200

    add logger in Validator

---
+++ src/main/java/com/softseve/migration/runner/AppRunner.java
@@ -0,0 +1,20 @@
+package com.softseve.migration.runner;
+
+import com.softseve.migration.converter.Converter;
+import com.softseve.migration.loader.Loader;
+import com.softseve.migration.processor.Processor;
+import com.softseve.migration.reader.CSVFileReader;
+import com.softseve.migration.watcher.FileType;
+import com.softseve.migration.watcher.Watcher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class AppRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Watcher watcher = new Watcher(
+            new Processor(new Loader(), new CSVFileReader(), new Converter()));
+        watcher.watch("CSVs/123/12", FileType.CSV);
+    }
+}
--- src/main/java/com/softseve/migration/validator/Validator.java
+++ src/main/java/com/softseve/migration/validator/Validator.java
@@ -1,15 +1,21 @@
 package com.softseve.migration.validator;
 
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Validator {
+
+    private static final Logger logger = LoggerFactory.getLogger(Validator.class);
 
     public String isValidUUID(String uuid, long line, String columnName) {
 
         try {
             UUID.fromString(uuid);
         } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Invalid " + uuid + " " + columnName);
+            logger.error(String
+                .format("Invalid value : %s in %s line in column %s", uuid, line, columnName));
+//            throw new RuntimeException("Invalid " + uuid + " " + columnName);
         }
         return uuid;
     }
@@ -19,7 +25,9 @@ public class Validator {
         try {
             Long.parseLong(number);
         } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Invalid " + number + " " + columnName);
+            logger.error(String
+                .format("Invalid value : %s in %s line in column %s", number, line, columnName));
+//            throw new RuntimeException("Invalid " + number + " " + columnName);
         }
         return number;
     }
