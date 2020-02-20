97c5569273312726b3f9a35ad335cc8f361248f0
iholovata <irina-golovataja@ukr.net>
2020-02-20 12:29:13 +0200

    Validator, Regexes added

---
+++ src/main/java/com/softseve/migration/validators/Regexes.java
@@ -0,0 +1,7 @@
+package com.softseve.migration.validators;
+
+public class Regexes {
+
+    public final static String UUID_REGEX = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
+    public final static String NAME_REGEX = "^[a-zA-Z]{1}[a-z]+$";
+}
---
+++ src/main/java/com/softseve/migration/validators/Validator.java
@@ -0,0 +1,8 @@
+package com.softseve.migration.validators;
+
+public class Validator {
+
+    public boolean isUUID(String s){
+        return true;
+    }
+}
