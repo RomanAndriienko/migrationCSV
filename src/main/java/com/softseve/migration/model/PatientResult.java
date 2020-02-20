f472ef2af43dca3198d66344fb5a1d7fb79ecc5a
iholovata <irina-golovataja@ukr.net>
2020-02-20 13:36:01 +0200

    PatientResult with fields, Source was added

--- src/main/java/com/softseve/migration/model/PatientResult.java
+++ src/main/java/com/softseve/migration/model/PatientResult.java
@@ -1,5 +1,37 @@
 package com.softseve.migration.model;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class PatientResult {
 
+    private UUID id;
+    private String cDate;
+    private String contactRef;
+    private String pCode;
+    private String firstName;
+    private String lastName;
+    private String user;
+    private int facility;
+    private int cntRef;
+    private int cntRef2;
+    private String contactTypeId;
+    private String contactType;
+    private UUID contactTypeIdx;
+    private int sums;
+    private String cContactDateTime;
+    private String uContactDateTime;
+
+    private String bDate;
+    private String refId;
+    private String accessDate;
+    private int items;
+    private int MPI;
+    private String patientTypeId;
+    private String patientTypeTxt;
+    private UUID patientTypeRef;
+    private String cPatientDateTime;
+    private String uPatientDateTime;
 }
