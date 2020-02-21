18ab267bb58a72d95fa483a657d5e1a3fc3f15dc
RomanAndriienko <andriienkoroman@gmail.com>
2020-02-21 12:07:30 +0200

    runner

--- src/main/java/com/softseve/migration/runner/AppRunner.java
+++ src/main/java/com/softseve/migration/runner/AppRunner.java
@@ -8,13 +8,18 @@ import com.softseve.migration.watcher.FileType;
 import com.softseve.migration.watcher.Watcher;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppRunner implements ApplicationRunner {
+
+//     specify directory for watcher with csv files
+//     add files in runtime
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Watcher watcher = new Watcher(
             new Processor(new Loader(), new CSVFileReader(), new Converter()));
-        watcher.watch(args.toString(), FileType.CSV);
+        watcher.watch("C:\\Users\\randr\\data", FileType.CSV);
     }
 }
